d51d339bb1408bfc38121f565400bd831441380b
yacine <87252026+ForzDz@users.noreply.github.com>
2025-05-19 00:04:43 +0100

    Update hero-content.tsx

--- components/sub/hero-content.tsx
+++ components/sub/hero-content.tsx
@@ -33,7 +33,7 @@ export const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Hey i'm{" "}
+            Hey im{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               the best
             </span>{" "}
@@ -45,7 +45,7 @@ export const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
+          Im a Full Stack Software Engineer with experience in Website,
           design, and front-end development. Check out my projects and skills.
         </motion.p>
 
